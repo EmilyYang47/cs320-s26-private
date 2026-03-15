0363c9f3a64f1762019dacee893c4caaf41e80a4
EmilyYang47 <eyang4@bu.edu>
2026-03-15 07:34:36 -0400

    Implemented eval

--- assigns/assign5/lib/assign5.ml
+++ assigns/assign5/lib/assign5.ml
@@ -21,74 +21,119 @@ let rec get_t env name =
     | [] -> None 
     | (n, t) :: rest_env -> if n = name then Some t else get_t rest_env name
 
+let rec find_size label lst =
+  match lst with
+  | [] -> None
+  | (l, s) :: rest -> if l = label then Some s else find_size label rest 
+
+let get_union a b = 
+  let sorted = List.sort (fun (x,_) (y,_) -> String.compare x y) (a @ b)
+  in 
+  let rec loop lst acc =
+      (match lst with
+      | [] -> Some (List.rev acc)  
+      | (l, s) :: rest -> ( match acc with
+                            | [] -> loop rest [(l, s)] 
+                            | (prev_l, prev_s) :: _ -> if l = prev_l then
+                                                          if s = prev_s then loop rest acc 
+                                                          else None
+                                                        else loop rest ((l, s) :: acc) 
+                          )
+      )
+  in
+  loop sorted [] 
+
+let rec remove_axis label lst =
+  match lst with
+  | [] -> []
+  | (l, s) :: rest -> if l = label then remove_axis label rest else (l, s) :: remove_axis label rest 
+
+let rec contains l labels = 
+  match labels with 
+  | [] -> false 
+  | x :: xs -> if l = x then true else contains l xs 
+
+
 let rec dim_check (env : (string * tensor) list) (e : expr) : ((string * int) list) option =
   (* TODO *)
   match e with 
   | Ident (name, labels) -> (match get_t env name with 
                             | None -> None 
                             | Some t -> if List.length (Tensor.shape t) <> List.length labels then None 
-                                        else if let rec contains l rest_labels = 
-                                                  (match rest_labels with 
-                                                  | [] -> false 
-                                                  | x :: xs -> if l = x then true else contains l xs 
-                                                  )
-                                                in 
-                                                let rec check_not_distinct label_lst =
+                                        else if let rec check_not_distinct label_lst =
                                                   (match label_lst with
                                                   | [] -> false
                                                   | x :: xs -> if contains x xs then true else check_not_distinct xs
                                                   )
                                                 in 
                                                 check_not_distinct labels then None 
-                                        else let rec generate_lst label_lst size_lst = 
-                                                (match (label_lst, size_lst) with 
-                                                | ([], []) -> [] 
-                                                | (x :: xs, y :: ys) -> (x, y) :: generate_lst xs ys 
-                                                | (_, _) -> [] 
-                                                )
-                                              in 
-                                              Some (generate_lst labels (Tensor.shape t))
+                                            else Some (Tensor.relabel_assoc (Tensor.idx_space t) labels)
                               )
   | Map (_, e1, e2) -> (match (dim_check env e1, dim_check env e2) with 
-                        | (Some a, Some b) -> let sorted = List.sort (fun (x,_) (y,_) -> String.compare x y) (a @ b)
-                                              in 
-                                              let rec loop lst acc =
-                                                  (match lst with
-                                                  | [] -> Some (List.rev acc)  
-                                                  | (l, s) :: rest -> ( match acc with
-                                                                        | [] -> loop rest [(l, s)] 
-                                                                        | (prev_l, prev_s) :: _ -> if l = prev_l then
-                                                                                                      if s = prev_s then loop rest acc 
-                                                                                                      else None
-                                                                                                    else loop rest ((l, s) :: acc) 
-                                                                      )
-                                                  )
-                                              in
-                                              loop sorted [] 
+                        | (Some a, Some b) -> get_union a b 
                         | (_, _) -> None 
                         )
   | Fold (_, i, e) -> match dim_check env e with 
                         | None -> None 
-                        | Some a -> let rec find_size label lst =
-                                      (match lst with
-                                      | [] -> None
-                                      | (l, s) :: rest -> if l = label then Some s else find_size label rest 
-                                      )
-                                    in 
-                                    (match find_size i a with 
+                        | Some a -> (match find_size i a with 
                                     | None -> None 
-                                    | Some _ -> let rec remove_axis label lst =
-                                                  match lst with
-                                                  | [] -> []
-                                                  | (l, s) :: rest -> if l = label then remove_axis label rest else (l, s) :: remove_axis label rest 
-                                                in Some (remove_axis i a)
-                                    )
-                                             
+                                    | Some _ -> Some (remove_axis i a)
+                                    ) 
 
 
+let rec eval (env : (string * tensor) list) (e : expr) : tensor =
+  (* TODO *) 
+  match e with 
+  | Ident (name, labels) -> (match get_t env name with
+                            | Some t -> Tensor.relabel_axes t labels
+                            | None -> assert false
+                            )
 
-let eval (env : (string * tensor) list) (e : expr) : tensor =
-  ignore (env, e); assert false (* TODO *)
+  | Map (op, e1, e2) -> let a = eval env e1 in 
+                        let b = eval env e2 in 
+                        let space = match get_union (Tensor.idx_space a) (Tensor.idx_space b) with 
+                                    | None -> assert false 
+                                    | Some x -> x
+                        in 
+                        let rec filter labels idx =
+                          (match idx with
+                          | [] -> []
+                          | (l, v) :: rest -> (if contains l labels then [(l, v)] else []) @ filter labels rest 
+                          )
+                        in 
+                        Tensor.init space (fun idx -> let idx1 = filter (Tensor.axis_labels a) idx
+                                                      in
+                                                      let idx2 = filter (Tensor.axis_labels b) idx
+                                                      in
+                                                      let v1 = Tensor.get a idx1 
+                                                      in
+                                                      let v2 = Tensor.get b idx2 
+                                                      in 
+                                                      match op with 
+                                                      | Add -> v1 +. v2 
+                                                      | Mul -> v1 *. v2 
+                                          )
+  | Fold (op, i, e) -> let a = eval env e in 
+                      let space = Tensor.idx_space a 
+                      in 
+                      let size = match find_size i space with
+                                | Some s -> s
+                                | None -> assert false
+                      in 
+                      let new_sp = remove_axis i space 
+                      in 
+                      Tensor.init new_sp (fun idx -> let rec loop j acc =
+                                                      if j = size then acc
+                                                      else
+                                                        let current =
+                                                          if j = 0 then Tensor.get a ((i,j)::idx)
+                                                          else match op with 
+                                                            | Add -> acc +. Tensor.get a ((i,j)::idx) 
+                                                            | Mul -> acc *. Tensor.get a ((i,j)::idx) 
+                                                        in loop (j+1) current
+                                                    in 
+                                                    loop 0 0.0
+                                          )
 
 type error =
   | Parse_error
