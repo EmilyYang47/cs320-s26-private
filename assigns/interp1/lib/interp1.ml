d23bbb40cb4360e8fa71971cac11bedd34a57d25
EmilyYang47 <eyang4@bu.edu>
2026-04-02 19:04:48 -0400

    Modified Bop in eval

--- assigns/interp1/lib/interp1.ml
+++ assigns/interp1/lib/interp1.ml
@@ -159,12 +159,12 @@ let eval (env : dyn_env) (e : expr) : value =
                                   | Eq  -> Bool (v1 = v2)
                                   | Neq -> Bool (v1 <> v2) 
                                   | _ -> assert false) 
-                            | Bool (v1), Bool (v2) ->
+                            | Bool (v1), v2 ->
                                 (match bop with
                                   (* | And -> Bool (v1 && v2) 
                                   | Or -> Bool (v1 || v2)  *)
-                                  | And -> if v1 = true then Bool(v2) else Bool(false) 
-                                  | Or -> if v1 = true then Bool(true) else Bool(v2)  
+                                  | And -> if v1 = true then v2 else Bool(false) 
+                                  | Or -> if v1 = true then Bool(true) else v2  
                                   | Eq  -> Bool (v1 = v2) 
                                   | Neq -> Bool (v1 <> v2) 
                                   | Lt -> Bool (v1 < v2)
