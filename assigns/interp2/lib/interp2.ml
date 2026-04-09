5d9ede6fd811214deae6d9ccab108466bb66215d
EmilyYang47 <eyang4@bu.edu>
2026-04-09 18:31:37 -0400

    Fixed and/or in eval

--- assigns/interp2/lib/interp2.ml
+++ assigns/interp2/lib/interp2.ml
@@ -359,10 +359,10 @@ let eval_expr (env : dyn_env) (e : expr) : value =
                                   | Or  -> if v1 then VBool(v1) else VBool(v2)
                                   | _ -> assert false
                                 )
-                            | Bool (v1), v2 ->
+                            | VBool (v1), v2 ->
                                 (match bop with
-                                  | And -> if v1 then v2 else v1
-                                  | Or  -> if v1 then v1 else v2
+                                  | And -> if v1 then v2 else VBool (false)
+                                  | Or  -> if v1 then VBool (true) else v2
                                   | _ -> assert false
                                 )
                             | v1, v2 -> 
